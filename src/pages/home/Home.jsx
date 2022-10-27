f55e47a4ec47b7073b0274a6886ec982c1c75f9c
ArcxSx <archanasharma2797@gmail.com>
2022-10-28 00:23:29 +0530

    completed searchbar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import Header from "../../components/header/Header"
 import Navbar from "../../components/navbar/Navbar"
 import "./home.css"
 
@@ -5,6 +6,7 @@ const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Header/>
     </div>
   )
 }
