92f05eae18d9162fd9bec973dca1157f7154d972
ArcxSx <archanasharma2797@gmail.com>
2022-10-27 21:48:58 +0530

    Revert "added nav, header, half of search bar"
    This reverts commit 7c4262b68db912cebb510048bd83f20d846c5f0b.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,15 +2,7 @@ import "./navbar.css"
 
 function Navbar() {
   return (
-    <div className="navbar">
-        <div className="navContainer">
-            <span className="logo">HotelX</span>
-            <div className="navItems">
-                <button className="navButton">Register</button>
-                <button className="navButton">Login</button>
-            </div>
-        </div>
-    </div>
+    <div>Navbar</div>
   )
 }
 
