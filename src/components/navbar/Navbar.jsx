a2de35f378afc659a7038ad4eaea7cc10d65f47a
ArcxSx <archanasharma2797@gmail.com>
2022-11-07 20:51:16 +0530

    search box color changed, gap fixed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
         <div className="navContainer">
-            <span className="logo">HotelX</span>
+            <span className="logo">LUXESTAY</span>
             <div className="navItems">
                 <button className="navButton">Register</button>
                 <button className="navButton">Login</button>
