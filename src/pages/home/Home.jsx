8328c53f4c42ffb084940658cf8fcee4db77d41b
Archana Sharma <74065315+ArcxSx@users.noreply.github.com>
2022-11-07 22:00:47 +0530

    Revert "Arch"

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
-import Navbar from "../../components/navbar/Navbar"
 import Header from "../../components/header/Header"
+import Navbar from "../../components/navbar/Navbar"
 import Featured from "../../components/featured/Featured"
 import PropertyList from "../../components/propertyList/PropertyList"
 import "./home.css"
